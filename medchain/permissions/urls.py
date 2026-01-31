c44e81af52f48bfc511a62abd201148afda9cb1e
Naseer-010 <naseer20704446@gmail.com>
2026-01-31 19:10:30 +0530

    remove all deleted folders

--- medchain/permissions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import GrantAccess, RevokeAccess
-
-urlpatterns = [
-    path("grant/", GrantAccess.as_view()),
-    path("revoke/", RevokeAccess.as_view()),
-]
--- medchain/records/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ToggleVisibility
-
-urlpatterns = [
-    path("records/<int:id>/visibility", ToggleVisibility.as_view()),
-]
