aa14af88bf832e804f6f319ccc106e0f73730c5f
Naseer-010 <naseer20704446@gmail.com>
2026-01-31 17:18:26 +0530

    implemented permissions access

--- medchain/permissions/models.py
+++ medchain/permissions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from medchain.accounts.models import User
 
 # Create your models here.
+
+class RecordAccess(models.Model):
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    granted_at = models.DateTimeField(auto_now_add=True)
